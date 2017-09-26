4971aab0e64f8ce225094feb711c4ca76e00d7ae
Sigri44 <sigri44@hotmail.fr>
2017-09-26 13:04:48 +0200

    correction typo

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form method="get" class="search-form" action="<?php bloginfo('home'); ?>/">
     <label>
         <span class="screen-reader-text">Rechercher&nbsp;:</span>
-        <input type="search" class="search-field" placeholder="Recherche…" value="<?php the_search_query(); ?>" name="s" title="Rechercher&nbsp;:" />
+        <input type="search" class="search-field" placeholder="Recherche …" value="<?php the_search_query(); ?>" name="s" title="Rechercher&nbsp;:" />
     </label>
     <input type="submit" class="search-submit" value="Rechercher" />
 </form>
